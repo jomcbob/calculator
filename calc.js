94c5da30569fb911877087ca5f78664fbbb602f7
andy <andrew@risertech.com>
2024-11-04 10:38:34 -0700

    added click functions to all number keys

---
+++ calc.js
@@ -0,0 +1,66 @@
+let num9 = document.querySelector(".num9")
+num9.addEventListener("click", ()  => {
+    document.getElementById("screen").value += "9";
+})
+
+let num8 = document.querySelector(".num8")
+num8.addEventListener("click", ()  => {
+    document.getElementById("screen").value += "8";
+})
+
+let num7 = document.querySelector(".num7")
+num7.addEventListener("click", ()  => {
+    document.getElementById("screen").value += "7";
+})
+
+let num6 = document.querySelector(".num6")
+num6.addEventListener("click", ()  => {
+    document.getElementById("screen").value += "6";
+})
+
+let num5 = document.querySelector(".num5")
+num5.addEventListener("click", ()  => {
+    document.getElementById("screen").value += "5";
+})
+
+let num4 = document.querySelector(".num4")
+num4.addEventListener("click", ()  => {
+    document.getElementById("screen").value += "4";
+})
+
+let num3 = document.querySelector(".num3")
+num3.addEventListener("click", ()  => {
+    document.getElementById("screen").value += "3";
+})
+
+let num2 = document.querySelector(".num2")
+num2.addEventListener("click", ()  => {
+    document.getElementById("screen").value += "2";
+})
+
+let num1 = document.querySelector(".num1")
+num1.addEventListener("click", ()  => {
+    document.getElementById("screen").value += "1";
+})
+
+let num0 = document.querySelector(".num0")
+num0.addEventListener("click", ()  => {
+    document.getElementById("screen").value += "0";
+})
+
+let plus = document.querySelector(".plus")
+plus.addEventListener("click", () => {
+    document.getElementById("screen").value += " + "
+})
+
+let result = document.querySelector(".result")
+result.addEventListener("click", () => {
+})
+
+
+let clear = document.querySelector(".clear")
+clear.addEventListener("click", () => {
+    document.getElementById("screen").value = "";
+})
+
+  
